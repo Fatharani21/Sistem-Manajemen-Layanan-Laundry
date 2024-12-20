915283eabb90fd0d6085f2bcdf98f9ab81b25784
fatha <fathaaaafj@student.uns.ac.id>
2024-12-20 23:57:35 +0700

    module

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from user_interface import tampilkan_halaman_awal
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Swift Clean")
-root.state=('zoomed')
-root.resizable(True, True)
+if _name_ == "_main_":
+    root = tk.Tk()
+    root.title("Swift Clean")
+    root.state('zoomed')  
+    root.resizable(True, True)  
 
-tampilkan_halaman_awal(root)
-
-root.mainloop()
+    tampilkan_halaman_awal(root)  
+    root.mainloop()
